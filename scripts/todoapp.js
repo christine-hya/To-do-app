e958a533dfb15624b81677dc0fa5adba74eed2a3
Xola dos Santos <xola.dossantos@xdode.com>
2021-07-01 19:44:21 +0200

    Update TODO script with new logic

--- scripts/todoapp.js
+++ scripts/todoapp.js
@@ -1,17 +1,25 @@
+
+
 class ToDoClass {
     constructor() {
-        this.tasks = JSON.parse(localStorage.getItem('TASKS'));
-        if (!this.tasks) {
-            this.tasks = [
-                { task: 'Practise violin', isComplete: false, date: date },
-                { task: 'Feed the cat', isComplete: true, date: date },
-            ];
-        }
+        let data = localStorage.getItem('TASKS');
+        this.tasks = []
 
-        this.loadTasks();
-        this.addEventListeners();
-        //this.updateNewTask();
+            if (!data) {
+                const p = document.createElement('p')
+                p.textContent = 'Nothing to do! Add a task?'
+                document.getElementById('list').append(p);
+
+            }
+        
+               else {
+                this.tasks = JSON.parse(localStorage.getItem('TASKS'));
+                this.loadTasks();    
     }
+               this.addEventListeners();
+}
+    //constructor end
+
 
     addEventListeners() {
         document.getElementById('addTask').addEventListener('keyup', event => {
@@ -20,16 +28,18 @@ class ToDoClass {
                 event.target.value = '';
             }
         });
-    }
+    };
+
 
     loadTasks() {
-        let tasksHtml = this.tasks.reduce((html, task, index) => html +=
-        this.generateTaskHtml(task, index, date), '');
+
+        let tasksHtml = this.tasks.reduce((html, task, index, date) => html +=
+            this.generateTaskHtml(task, index, date), '');
         document.getElementById('list').innerHTML = tasksHtml;
         localStorage.setItem('TASKS', JSON.stringify(this.tasks));
     }
 
-    generateTaskHtml(task, index, date) {
+    generateTaskHtml(task, index, id) {
         return ` <li>
         <div class="row py-1 px-2 mx-auto">
     
@@ -39,15 +49,18 @@ class ToDoClass {
         
             </div>
     
-            <div class="col-8">
-                <p id="taskDisplay" class="${task.isComplete ? 'complete' : ''} replace-with-edit mx-0 todo-style">${task.task} ${date.date}</p>
-                <input type="text" placeholder="${task.task}" id="editTaskField" class="hide">
+            <div class="col-8 input-container" onClick="toDo.editTask(event, ${undefined})">
+              <input type="text" data-id="${id}" class="${task.isComplete ? 'complete' : ''} mx-0 todo-style" input value="${task.task}" disabled>
+                
             </div>
-    
-            <div class="col-1 ps-0">
-            <button id="edit-button" class="button-style-list" onClick="toDo.editTask()";><i class="far fa-edit"></i> </button>
 
-        </div>
+              
+            <div class="col-2 ps-0">
+                <button class="button-style-list" onClick="toDo.editTask(event, ${index})">
+                edit
+                </button>
+            </div>
+
            
     
             <div class="col-2 ps-0">
@@ -76,17 +89,18 @@ class ToDoClass {
     addTaskClick() {
         let target = document.getElementById('addTask');
         this.addTask(target.value);
-        target.value = ""
+        target.value = "";
     }
 
     addTask(task) {
         let dueDate = document.getElementById("due-date").value;
-        let dateString = dueDate.toString()
+        let dateString = JSON.stringify(dueDate);
         let newTask = {
             task,
             isComplete: false,
             date: dateString
         };
+
         let addWarning = document.getElementById('addTask');
         if (task === '') {
             addWarning.classList.add('border');
@@ -98,46 +112,87 @@ class ToDoClass {
             addWarning.classList.remove('border-4');
             this.tasks.push(newTask);
             this.loadTasks();
+           
         }
     }
 
-    //clearTasks() {
-    //localStorage.clear();
-    //}
+    clearTasks() {
+       localStorage.clear();
+       location. reload();
+    }
 
     //edit task item
-    editTask() {
+    editTask(event, index) {
 
-        document.getElementById("editTaskField").style.display = "block";
-        document.getElementById("taskDisplay").style.display = "none";
-        this.updateNewTask();
+        let taskInput = event.target;
 
-    }
+        let _data; 
 
-    updateNewTask() {
-        let newValue = document.getElementById("editTaskField").value;
-        document.getElementById("editTaskField").addEventListener('keypress', function (e) {
-            if (e.key === 'Enter') {
-                toDo.tasks.push(newValue);
-                toDo.loadTasks();
+        // Copy the current to do tasks into the private _data array
+        if (index !== undefined) {
+
+            _data = this.tasks; 
+
+            _data[index] = taskInput.innerHTML; 
+    
+            // Step 3 Write the contents of _data to loccal storage 
+
+            localStorage.setItem('TASKS', JSON.stringify(_data));
+
+    
+            // Force load tasks 
+    
+            // loadTasks 
+
+        }
+        
+
+        if (taskInput.disabled == true) {
+            taskInput.disabled = !taskInput.disabled;
+
+        }
+
+        taskInput.addEventListener('keyup', event => {
+            if (event.key === 'Enter') {
+
+                let newValue = event.target.value;
+                taskInput.innerHTML = newValue;
+                taskInput.disabled = true;
+                // this.tasks.task = newvalue;
+                // this.addToLocalStorageArray("TASKS", newvalue);
+
+                let editedTask = {
+                    // id: this.id++,
+                     task: `${newValue}`,
+                     isComplete: false,
+                     date: date
+                 }
+
+            //    this.tasks.push(editedTask);
+               localStorage.setItem('TASKS', JSON.stringify(this.tasks));
+
+               //load tasks call 
+              
+
+
+               //JSON.stringify(localStorage.setItem("TASKS", `${newvaluestring}`));
+             
             }
-        })
+        });
     }
+
+
 
     //sort alphabetically
     sortAlphabetically() {
-        let sortedTodos = this.tasks.map(el => this.tasks[el.index]);
-        this.loadTasks(sortedTodos);
-        return this.tasks
-            .sort((a, b) => {
-                if (a.value > b.value) { return 1; }
-                if (a.value < b.value) { return -1 }
-                return 0;
-            })
-
+        
+        this.tasks.sort((a, b) => a.task.localeCompare(b.task))
+        console.log(this.tasks);
+        this.loadTasks();
     }
 
 }
+//class end
 
 let toDo;
 window.addEventListener("load", function () {
@@ -150,4 +205,9 @@ const dateElement = document.getElementById("date");
 const options = { weekday: "long", month: "short", day: "numeric" };
 const today = new Date();
 dateElement.innerHTML = today.toLocaleDateString("en-GB", options);
+
+//function addDate() {
+   // let dueDate = document.getElementById("due-date").value;
+    
+//}
 
